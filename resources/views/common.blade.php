7dd115e47278a5e2c31789033222a32a058399a8
Yamato-Suzuki <suzuki.yamato@ad-kit.co.jp>
2020-08-26 17:26:26 +0900

    マスターより リスト作成

---
+++ resources/views/common.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>京都へ行こう</title>
+</head>
+<body>
+    <header>
+
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">料金のご案内</a></li>
+            <li><a href="#">車両のご案内</a></li>
+            <li><a href="#">ご利用事例</a></li>
+            <li><a href="#">よくある質問</a></li>
+            <li><a href="#">お問い合わせ</a></li>
+            <li><a href="#">会社概要</a></li>
+        </ul>
+
+    </header>
+ 
+     
+
+</body>
+</html>
---
+++ resources/views/users/mypage.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+    <h2>マイページ</h2>
+
+    <p>名前：{{ $users->name }}</p>
+
+    <p>メールアドレス：{{ $users->email }}</p>
+    <p>写真：{{ $users->img}}</p>   
+    
+    {{-- <p>パスワード：{{ $user->password}}</p> --}}
+ 
+    {{-- {{ $user->created_at }}</p> --}}
+    {{-- 名前→{{$profile->name}} --}}
+        {{-- プロフィール写真→{{$profile->image}} --}}
+        {{-- 性別→{{$profile->gender}}
+        居住地→{{$profile->prefecture}} --}}
+    <a class="btn btn-success" href="{{ route('users.edit', ['id' => $users->id]) }}">編集</a>
+ 
+    <pre>
+        {{-- <h1>{{ $msg }}</h1> --}}
+ 
+    <table border="1">
+    <tr>
+        <th>⭐️行きたいリスト</th>
+        <th>⭐️行ったリスト</th>
+    </tr>
+    <tr>
+        <th>{{ $visit_spots }}</th>
+        <th>{{ $want_spots }}</th>
+    </tr>
+    {{-- @foreach ($books as $book)
+    <tr>
+        <td>{{ $book->title }}</td>
+        <td>{{ $book->author }}</td>
+    </tr>
+    @endforeach --}}
+</table>
+</body>
+</html>
